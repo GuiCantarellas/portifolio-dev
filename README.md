35b55c9e5812637645d07edc7abbf1de65fbcab2
GuiCantarellas <86744375+GuiCantarellas@users.noreply.github.com>
2025-09-23 16:34:16 +0000

    adiciona scripts `lint:check e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # portifolio-dev
+
 Colocar em pratica o que estou aprendendo com os cursos que estou fazendo junto aos conhecimentos do meu emprego estagiando pela primeira vez na área
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home (){
-    return <h1>Se chegou aqui você está devendo uma cerveja para o Guilherme Cantarellas</h1>
+function Home() {
+  return (
+    <h1>
+      Se chegou aqui você está devendo uma cerveja para o Guilherme Cantarellas
+    </h1>
+  );
 }
 
 export default Home;
